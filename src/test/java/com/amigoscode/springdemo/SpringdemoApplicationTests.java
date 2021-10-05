f0e09f42fb929786424127b1b75663b5c4402930
Krisz1987 <krisztian55@gmail.com>
2021-10-05 12:11:56 +0200

    Initial commit

---
+++ src/test/java/com/amigoscode/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigoscode.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
